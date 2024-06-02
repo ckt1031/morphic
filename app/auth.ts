c6b3b1ce311edd5852e3877fde8654ab34a9c285
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-02 05:01:38 +0000

    feat: Add ZITADEL as authentication provider

--- app/auth.ts
+++ app/auth.ts
@@ -1,12 +1,14 @@
 import NextAuth from 'next-auth'
 import Auth0 from 'next-auth/providers/auth0'
+import ZITADEL from "next-auth/providers/zitadel";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Auth0({
-      issuer: process.env.AUTH_AUTH0_ISSUER,
-      clientId: process.env.AUTH_AUTH0_ID,
-      clientSecret: process.env.AUTH_AUTH0_SECRET
-    })
+    ZITADEL,
+    // Auth0({
+    //   issuer: process.env.AUTH_AUTH0_ISSUER,
+    //   clientId: process.env.AUTH_AUTH0_ID,
+    //   clientSecret: process.env.AUTH_AUTH0_SECRET
+    // })
   ]
 })
